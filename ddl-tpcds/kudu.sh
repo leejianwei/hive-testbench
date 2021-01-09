70099b50d9e9c77f9be7af1eb0f4a283d1ac2ac4
Jianwei Li <lijianwe@amazon.com>
2021-01-09 17:14:49 +0800

    update script for Presto

---
+++ ddl-tpcds/kudu.sh
@@ -0,0 +1,14 @@
+DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
+FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
+
+for t in ${DIMS}
+do
+	presto-cli --catalog kudu --schema default -f ${t}
+	echo "create Presto table over Kudu: " ${t}
+done
+
+for t in ${FACTS}
+do
+	presto-cli --catalog kudu --schema default -f ${t}
+	echo "create Presto table over Kudu: " ${t}
+done
