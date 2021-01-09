18274a79ee5f8093d21bcad89a42781dad3a4e89
Jianwei Li <lijianwe@amazon.com>
2021-01-09 22:14:13 +0800

    update table ranger partition

--- ddl-tpcds/kudu/catalog_returns.sql
+++ ddl-tpcds/kudu/catalog_returns.sql
@@ -32,10 +32,36 @@ create table catalog_returns(
 )
 WITH (
   partition_by_range_columns = ARRAY['cr_returned_date_sk'],
-  range_partitions = '[{"lower": null, "upper": 2450},
-                       {"lower": 2450, "upper": 2451},
-                       {"lower": 2451, "upper": 2452},
-                       {"lower": 2452, "upper": null}]'
+  range_partitions = '[{"lower": null, "upper": 24501},
+                       {"lower": 24501, "upper": 24502},
+                       {"lower": 24502, "upper": 24503},
+                       {"lower": 24503, "upper": 24504},
+                       {"lower": 24504, "upper": 24505},
+                       {"lower": 24505, "upper": 24506},
+                       {"lower": 24506, "upper": 24507},
+                       {"lower": 24507, "upper": 24508},
+                       {"lower": 24508, "upper": 24509},
+                       {"lower": 24509, "upper": 24510},
+                       {"lower": 24510, "upper": 24511},
+                       {"lower": 24511, "upper": 24512},
+                       {"lower": 24512, "upper": 24513},
+                       {"lower": 24513, "upper": 24514},
+                       {"lower": 24514, "upper": 24515},
+                       {"lower": 24515, "upper": 24516},
+                       {"lower": 24516, "upper": 24517},
+                       {"lower": 24517, "upper": 24518},
+                       {"lower": 24518, "upper": 24519},
+                       {"lower": 24519, "upper": 24520},
+                       {"lower": 24520, "upper": 24521},
+                       {"lower": 24521, "upper": 24522},
+                       {"lower": 24522, "upper": 24523},
+                       {"lower": 24523, "upper": 24524},
+                       {"lower": 24524, "upper": 24525},
+                       {"lower": 24525, "upper": 24526},
+                       {"lower": 24526, "upper": 24527},
+                       {"lower": 24527, "upper": 24528},
+                       {"lower": 24528, "upper": 24529},
+                       {"lower": 24529, "upper": null}]'
 );
 
 insert into catalog_returns
